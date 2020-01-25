ed83bc76f3a9d28ee6f34103f45942c50946650e
yuuma-S <y.sakurai2206@gmail.com>
2020-01-25 14:50:05 +0000

    routing add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: 'questions#index'
+get '/about' => 'home#about'
+
+# adminのルーティング
+namespace :admin do
+	resources :end_users, only: [:index, :show, :destroy]
+
+	resources :questions, only: [:index, :show, :destroy] do
+	    resources :comments, only: [:destroy]
+	end
+
+	resources :answers, only: [:show, :destroy] do
+
+	end
+end
+
+# end_userのルーティング
+resources :end_users do
+    resource :relationships, only: [:create, :destroy]
+      get :follows, on: :member
+      get :followers, on: :member
+end
+
+resources :questions, except: [:update] do
+    resources :comments, only: [:create, :destroy]
+    resources :answers, except: [:index] do
+  		resource :favorites, only: [:create, :destroy]
+	end
+end
+
 end
