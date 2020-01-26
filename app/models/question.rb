28c82fbb700b2f508ef6e914c9ed55a515015101
yuuma-S <y.sakurai2206@gmail.com>
2020-01-26 08:40:17 +0000

    model association add

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+
+	belongs_to :end_user
+	has_many :answers
+	has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_161639) do
+ActiveRecord::Schema.define(version: 2020_01_26_082432) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_161639) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "question_id"
   end
 
   create_table "comments", force: :cascade do |t|
