cb14bb3a6f760262079d3ffa7989baa78ef76c78
yuuma-S <y.sakurai2206@gmail.com>
2020-02-07 04:33:38 +0000

    validation add & error partial template add

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
 	belongs_to :question
 	belongs_to :end_user
 
+	# =============== validate ======================
+	# validates :comment, presence: true
+	# ===============================================
+
 end
