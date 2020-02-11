66cea31fea57b2e653f3d3a48488d04cc305eb4a
yuuma-S <y.sakurai2206@gmail.com>
2020-02-11 04:56:01 +0000

    answer new view rayout

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
 	has_many :favorites
 
 	# =============== validate =================
-	validates :title, presence: true
+	validates :title, presence: true, length: {maximum: 200, miximum: 10}
 	validates :body ,presence: true, length: {maximum: 3000}
 
 	# ==========================================
