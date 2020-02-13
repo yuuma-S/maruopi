ca12b8cc3496cb43eeeb802d5f028fec00a9462c
yuuma-S <y.sakurai2206@gmail.com>
2020-02-13 11:33:57 +0000

    migration error

--- db/migrate/20200201163331_remove_question_from_questions.rb
+++ db/migrate/20200201163331_remove_question_from_questions.rb
@@ -1,6 +1,5 @@
 class RemoveQuestionFromQuestions < ActiveRecord::Migration[5.2]
   def change
-    remove_column :questions, :question_id, :interger
     add_column :questions, :answer_id, :interger
   end
 end
