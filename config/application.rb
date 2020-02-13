5320bb362a8dbdea7059af3e2fca2151a2d8f09a
yuuma-S <y.sakurai2206@gmail.com>
2020-02-13 10:44:15 +0000

    error modify

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,11 @@ module Maruopi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips = '10.0.2.2'
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    unless Rails.env.production?
+      config.web_console.whitelisted_ips = '10.0.2.2'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
