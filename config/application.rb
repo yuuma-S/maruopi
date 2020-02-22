aab3ef548f15d7677bfe7387fc5d36a64901e081
yuuma-S <y.sakurai2206@gmail.com>
2020-02-22 09:29:23 +0000

    gem rails i18n install

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Maruopi
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       html_tag
     end
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     unless Rails.env.production?
       config.web_console.whitelisted_ips = '10.0.2.2'
     end
